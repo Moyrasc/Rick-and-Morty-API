f6f4d6a5507b523fd4fdbb62148a206a409abeb5
Moyra_sc <noemisolac@gmail.com>
2022-08-04 19:25:10 +0000

    lógica view personajes terminada

--- src/js/views/characters.jsx
+++ src/js/views/characters.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 
  const Characters = () => {
 
+
   const [person, setPerson] = useState([])
   const {id} = useParams()
   
@@ -22,34 +23,23 @@ import { useParams } from "react-router-dom";
   // console.log(id)
   return (
     <>
-      <div className="mainContainer mt-5">
-        <div className="card mb-3 border-none">
-          <div className="row g-0">
-            <div className="col-md-4">
-              <img
-                src={person.image}
-                className="img-fluid rounded-start"
-                alt="..."
-              />
-            </div>
-            <div className="col-md-8 card-body">
-              <div className="card-body">
-                <h1 className="card-title text-dark text-center">
-                  {person.name}
-                </h1>
-                <p className="card-text text-dark text-center">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s, when an unknown
-                  printer took a galley of type and scrambled it to make a type
-                  specimen book. It has survived not only five centuries, but
-                  also the leap into electronic typesetting, remaining
-                  essentially unchanged. It was popularised in the 1960s with
-                  the release of Letraset sheets containing Lorem Ipsum
-                  passages, and more recently
-                </p>
-              </div>
-            </div>
+      <div className="container-fluid d-flex justify-content-center mt-4" key={person.id}>
+        <div className="card" style={{ width: 30 + "rem" }}>
+          <img src={person.image} className="card-img-top" alt="..." />
+          <div className="card-body border text-center">
+            <h5 className="card-title">{person.name}</h5>
+             <p className="card-text m-1">
+              {" "}
+              <strong>Status:</strong> {person.status}{" "}
+            </p>
+            <p className="card-text m-1">
+              {" "}
+              <strong>Species:</strong> {person.species}
+            </p>
+            <p className="card-text ">
+              {" "}
+              <strong>Gender:</strong> {person.gender}
+            </p>
           </div>
         </div>
       </div>
