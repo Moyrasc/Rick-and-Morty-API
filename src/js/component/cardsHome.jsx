8369442a6951af172629c36558ba5050784b3ccb
Moyra_sc <noemisolac@gmail.com>
2022-08-06 15:00:45 +0000

    working on responsive card

--- src/js/component/cardsHome.jsx
+++ src/js/component/cardsHome.jsx
@@ -14,7 +14,7 @@ const CardsHome = () => {
         <Autocomplete/>
       </div>
         <h2 className="row-title  mb-4 text-white">Characters</h2>
-        <div className="d-flex justify-content-start ">
+        <div className="row row-cols-1 row-cols-md-4 row-col-lg">
           <CardCharacter/>
         </div>
         <h2 className="row-title  mb-4 mt-4 text-white">Locations</h2>
